e9af18829f5ff45f7bcfe5e607d8d026ee97a3c4
Steve Pederson <stephen.pederson.au@gmail.com>
2022-06-15 23:10:46 +0930

    Checked up to diff binding

--- workflow/scripts/create_pairwise_comparison.R
+++ workflow/scripts/create_pairwise_comparison.R
@@ -2,10 +2,10 @@ library(tidyverse)
 library(glue)
 
 args <- commandArgs(TRUE)
-target1 <- args[[1]]
+source1 <- args[[1]]
 ref1 <- args[[2]]
 treat1 <- args[[3]]
-target2 <- args[[4]]
+source2 <- args[[4]]
 ref2 <- args[[5]]
 treat2 <- args[[6]]
 threads <- args[[7]]
@@ -14,7 +14,7 @@ rmd <- args[[8]]
 glue(
   "
 	---
-	title: '{{target1}}: {{treat1}} Vs. {{ref1}} Compared To {{target2}}: {{treat2}} Vs. {{ref2}}'
+	title: '{{source1}}: {{treat1}} Vs. {{ref1}} Compared To {{source2}}: {{treat2}} Vs. {{ref2}}'
 	date: \"`r format(Sys.Date(), '%d %B, %Y')`\"
 	bibliography: references.bib
 	link-citations: true
@@ -26,8 +26,8 @@ glue(
 	```{r set-vals}
 	threads <- {{threads}}
 	pairs <- list(
-	  \"{{target1}}\" = c(\"{{ref1}}\", \"{{treat1}}\"),
-	  \"{{target2}}\" = c(\"{{ref2}}\", \"{{treat2}}\")
+	  \"{{source1}}\" = c(\"{{ref1}}\", \"{{treat1}}\"),
+	  \"{{source2}}\" = c(\"{{ref2}}\", \"{{treat2}}\")
 	)
 	```
 
